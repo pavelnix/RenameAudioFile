6c3b1d336bb80d9a050b4310a90362a23c4f8943
Pavel <pavelnix@yandex.ru>
2016-04-01 18:29:41 -0700

    1

--- src/RenameAudioFile.java
+++ src/RenameAudioFile.java
@@ -1,15 +1,30 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
  * Created by Administrator on 01.04.2016.
  */
 public class RenameAudioFile {
+
     public static void main(String[] args) throws FileNotFoundException {
-        System.out.println(args[0]);
+
         Scanner scanner = new Scanner(new File(args[0]));
+
+        File[] filesList;
+        File filesPath = new File("."); // создаем объект на папку с файлами
+        filesList = filesPath.listFiles(); // записываем файлы из папки в массив объектов типа File
+        for(File file : filesList){
+            String str = file.getName();
+            if( (str.endsWith(".mp3") && (!str.endsWith("..mp3")))){
+                    System.out.println(str);
+                str = str.replaceFirst(".mp3", "..mp3");
+                file.renameTo(new File(str));
+            }
+        }
+
         while (scanner.hasNext()){
             int number = scanner.nextInt();
             System.out.println(number);
@@ -27,5 +42,5 @@ public class RenameAudioFile {
 
             System.out.println();
         }
-     }
-}
+    }
+ }
